c78f7e3c1e3dc3de3070ae98ffc700ea3261a054
Frank Mathy <frankmathy@gmail.com>
2018-03-10 17:01:07 +0100

    Basic view controller

---
+++ CloudKitSycTasks/TaskListTableViewController.swift
@@ -0,0 +1,110 @@
+//
+//  TaskListTableViewController.swift
+//  CloudKitSycTasks
+//
+//  Created by Frank Mathy on 10.03.18.
+//  Copyright © 2018 Frank Mathy. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class TaskListTableViewController: UITableViewController {
+    
+    var tasks = [Task]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        self.navigationItem.leftBarButtonItem = self.editButtonItem
+    }
+    
+    func reloadData() {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+        let context = appDelegate.persistentContainer.viewContext
+        let taskFetch = NSFetchRequest<Task>(entityName: "Task")
+        do {
+            tasks = try context.fetch(taskFetch)
+            self.tableView.reloadData()
+        } catch let error as NSError {
+            print("Error loading tasks: \(error), \(error.userInfo)")
+            return
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TaskCell", for: indexPath)
+        let task = tasks[indexPath.row]
+        cell.textLabel?.text = task.taskName
+        if task.done {
+            cell.detailTextLabel?.text = "Done"
+        } else {
+            cell.detailTextLabel?.text = "Todo"
+        }
+        return cell
+    }
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
