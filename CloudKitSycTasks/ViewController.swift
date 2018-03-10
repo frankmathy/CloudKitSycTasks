64282f57f8e2d646c250d4e6ee1c203c2bae12fa
Frank Mathy <frankmathy@gmail.com>
2018-03-10 16:38:39 +0100

    Initial Commit

---
+++ CloudKitSycTasks/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CloudKitSycTasks
+//
+//  Created by Frank Mathy on 10.03.18.
+//  Copyright © 2018 Frank Mathy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
